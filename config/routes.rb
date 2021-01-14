f64563c3daa817e8205da757ac5260265d87ffbb
fujiidan <2607dan@gmail.com>
2021-01-15 08:15:58 +0900

    ウィザード形式でのユーザー登録実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'profiles', to: 'users/registrations#new_profile'
+    post 'profiles', to: 'users/registrations#create_profile'
+  end
+
 end
