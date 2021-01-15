dacbdd034dc61c387ea46689db383e6f54500cbb
fujiidan <2607dan@gmail.com>
2021-01-15 14:14:48 +0900

    rspec導入　factorybot作成（user,profile)

---
+++ spec/factories/profiles.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :profile do
+    address       {Gimei.address.kangi}
+    age           {rand(1..100)}
+    sex_id        {rand(1..2)}
+    association :user
+  end
+end
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    
+    nickname                 {Gimei.name}
+    email                    {Faker::Internet.free_email}
+    password                 { "1a#{Faker::Internet.password(min_length: 4, mix_case: false)}" }
+    password_confirmation    { password }
+  end
+end
