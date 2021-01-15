d9115eabe194ed1582770ca6c720fd41b960379b
fujiidan <2607dan@gmail.com>
2021-01-15 12:43:50 +0900

    パスワードのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, uniqueness: true
-  
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は英字と数字を１文字以上含めて下さい' }
+
   has_one :profile, dependent: :destroy
 end
