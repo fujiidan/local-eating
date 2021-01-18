a4086648cbf91c931c4051ec7c68c0e338980ca7
fujiidan <2607dan@gmail.com>
2021-01-18 18:13:56 +0900

    バリデーション変更

--- app/models/store.rb
+++ app/models/store.rb
@@ -3,7 +3,7 @@ class Store < ApplicationRecord
   validates :latitude, :longitude, numericality: { message: 'が算出されません、適切な住所を入力してください' }
   validates :genre_id, :price_id, numericality: { only_integer: true, message: 'を選択してください' }
   geocoded_by :address
-  after_validation :geocode
+  before_validation :geocode
 
   belongs_to :user
   has_many_attached :images
