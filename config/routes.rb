7dfb4484c91681f55c420f2b2370f33fa4a16bc1
fujiidan <2607dan@gmail.com>
2021-01-22 09:05:42 +0900

    usersコントローラー作成及びルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
     get 'profiles', to: 'users/registrations#new_profile'
     post 'profiles', to: 'users/registrations#create_profile'
   end
+
+  resources :users, only: [:show, :edit, :update, :destroy]
+
   resources :stores  do
     collection do
       get 'search_map'
