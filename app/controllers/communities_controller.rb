270b810ec969b0cb482cd05b77db93eb82d7460d
fujiidan <2607dan@gmail.com>
2021-01-25 17:11:07 +0900

    コミュニテイ作成機能実装

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,6 +1,25 @@
 class CommunitiesController < ApplicationController
 
   def index
+    @community = Community.new
     @communities = Community.order('created_at DESC')
+  end
+
+  def create
+    @community = Community.new(community_params)
+    @communities = Community.order('created_at DESC')
+    respond_to do |format|
+      if @community.save
+        format.js
+      else 
+        format.html { render template: 'communities/index.html.erb' }
+      end
+    end    
+  end
+
+  private
+
+  def community_params
+    params.require(:community).permit(:name).merge(user_id: current_user.id)
   end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
     resources :comments, only: :create
   end
 
-  resources :communities, only: [:index]
+  resources :communities, only: [:index, :create, :edit, :update, :destroy]
 end
