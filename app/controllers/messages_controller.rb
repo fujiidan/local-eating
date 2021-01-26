5f699fc47f21a47d944abbe5e5b7da790be5fa70
fujiidan <2607dan@gmail.com>
2021-01-26 19:38:29 +0900

    メッセージ投稿実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,5 +7,21 @@ class MessagesController < ApplicationController
   end
   
   def create
+    @community = Community.find(params[:community_id])
+    @message = @community.messages.new(message_params)
+    respond_to do |format|
+      if @message.save
+        format.js
+      else 
+        @messages = @community.messages.includes(:user).order("created_at DESC")
+        format.html { render template: 'messages/index.html.erb' }
+      end
+    end    
+  end
+  
+  private
+
+  def message_params
+    params.require(:message).permit(:message, message_images: []).merge(user_id: current_user.id)
   end  
 end
