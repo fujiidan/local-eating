6f9d468823672ae73c3e64a74e561f5f8fbb756d
fujiidan <2607dan@gmail.com>
2021-01-26 19:02:33 +0900

    message indexビュー実装（仮）

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,9 @@
 class MessagesController < ApplicationController
 
   def index
+    @community = Community.find(params[:community_id])
+    @message = Message.new
+    @messages = @community.messages.includes(:user).order("created_at DESC")
   end
   
   def create
