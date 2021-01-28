153244459cc76e107ee1283df1aa698b5c11306f
fujiidan <2607dan@gmail.com>
2021-01-28 09:05:23 +0900

    map.jsのURL条件分け追加

--- app/javascript/map.js
+++ app/javascript/map.js
@@ -1,7 +1,7 @@
 // googleMapの初期位置を設定トップページ、店舗詳細ページ、マイページ
 function initMap(){
   // 店舗詳細ページの場合の条件分岐
-  if (gon.store) {
+  if (document.getElementById('store-map')) {
     let mapInstance = new google.maps.Map(document.getElementById('store-map'), {
       center: {lat: gon.store.latitude, lng: gon.store.longitude},
       zoom: 16
@@ -17,8 +17,8 @@ function initMap(){
         </div>`
       });
       infoWindow.open(mapInstance, marker);
-      // マイページ場合の条件分岐
-  } else if (gon.profile) {
+    // マイページ場合の条件分岐
+  } else if (document.getElementById('user-map')) {
       mapInstance = new google.maps.Map(document.getElementById('user-map'), {
       center: {lat: gon.profile.latitude, lng: gon.profile.longitude},
       zoom: 15
@@ -33,11 +33,11 @@ function initMap(){
         </div>`
       });
       infoWindow.open(mapInstance, marker);
-      // トップページ場合の条件分岐
+    // トップページ場合の条件分岐
   } else {
-    mapInstance = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 35.662, lng: 139.704},
-    zoom: 15
+      mapInstance = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: 35.662, lng: 139.704},
+      zoom: 15
     });
   }
 }
@@ -60,5 +60,9 @@ function allMap () {
       });
   });
 };
-window.addEventListener("load", initMap);
-window.addEventListener("load", allMap);
+if (location.pathname.match(/users.+\d$/) || location.pathname.match(/stores.+\d$/) || location.pathname.match(/^\/$/)) {
+  window.addEventListener("load", initMap, allMap);
+}
+if (location.pathname.match(/users.+\d$/) || location.pathname.match(/^\/$/)) {
+  window.addEventListener("load", allMap);
+}
