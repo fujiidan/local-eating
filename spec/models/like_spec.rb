b485af97bbbc37fbe91c7961d77801e6f7fbfa69
fujiidan <2607dan@gmail.com>
2021-01-29 09:51:32 +0900

    likes単体　結合テスト実施

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @like = FactoryBot.build(:like)
+  end
+  
+  describe 'いいね機能' do
+
+    it 'いいねを保存するにはuser_idとstore_idが必須であること' do
+      @like.save
+      expect(@like.user_id).to eq 1
+      expect(@like.store_id).to eq 1
+    end
+  end  
+
 end
