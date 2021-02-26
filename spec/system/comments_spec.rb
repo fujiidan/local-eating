f8e73ccff8f409c1904208a8c1054bfb4eefc794
fujii dan <2607dan@gmail.com>
2021-02-26 19:18:20 +0900

    Commnet結合テスト追加

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -51,21 +51,65 @@ RSpec.describe 'Comments', type: :system do
     before do
       @store = FactoryBot.create(:store)
       @profile = FactoryBot.create(:profile, user_id: @store.user_id)
-      FactoryBot.create_list(:comment, 3, store_id: @store.id, user_id: @store.user_id)
+      FactoryBot.create(:comment, store_id: @store.id, user_id: @store.user_id)
     end
 
-    it 'ユーザーアカウントが削除されるとそれに紐づくコメント情報も削除されること' do
-      sign_in(@store.user)
-      visit user_path(@store.user)
-      expect { click_on('アカウント削除') }.to change { @store.user.comments.count }.by(-3)
-      expect(current_path).to eq root_path
+    context 'コメント削除できる時' do
+
+      it 'コメント作成者には削除ボタンが表示されること' do
+        sign_in(@store.user)
+        visit store_path(@store)
+        expect(page).to have_css(".fa-trash-alt")
+      end
+
+      it '店舗作成者は自身が作成していないコメントでも削除ボタンが表示されること' do
+        @another_store = FactoryBot.create(:store)
+        another_profile = FactoryBot.create(:profile, user_id: @another_store.user_id)
+        FactoryBot.create(:comment, store_id: @another_store.id, user_id: @store.user_id)
+        sign_in(@another_store.user)
+        visit store_path(@another_store)
+        expect(page).to have_css(".fa-trash-alt")
+      end  
+
+      # # docker環境でjs動かない為一旦コメントアウト
+      # it 'コメント削除するとページ遷移なく画面に留まること', js: true  do
+      #   sign_in(@store.user)
+      #   visit store_path(@store)
+      #   find(".fa-trash-alt").click
+      #   sleep(1)
+      #   expect(Comment.count).to eq 0
+      # end
+
+      it 'ユーザーアカウントが削除されるとそれに紐づくコメント情報も削除されること' do
+        FactoryBot.create_list(:comment, 2, store_id: @store.id, user_id: @store.user_id)
+        sign_in(@store.user)
+        visit user_path(@store.user)
+        expect { click_on('アカウント削除') }.to change { @store.user.comments.count }.by(-3)
+        expect(current_path).to eq root_path
+      end
+
+      it '店舗情報を削除するとそれに紐づくコメントも削除されること' do
+        FactoryBot.create_list(:comment, 2, store_id: @store.id, user_id: @store.user_id)
+        sign_in(@store.user)
+        visit store_path(@store)
+        expect { click_on('削除する') }.to change { @store.comments.count }.by(-3)
+        expect(current_path).to eq user_path(@store.user)
+      end
     end
 
-    it '店舗情報を削除するとそれに紐づくコメントも削除されること' do
-      sign_in(@store.user)
-      visit store_path(@store)
-      expect { click_on('削除する') }.to change { @store.comments.count }.by(-3)
-      expect(current_path).to eq user_path(@store.user)
-    end
+    context 'コメント削除できない時' do
+
+      it 'ログインしていないユーザーには削除ボタンが表示されないこと' do
+        visit store_path(@store)
+        expect(page).to have_no_css(".fa-trash-alt")
+      end
+
+      it 'ログイン済みでもコメント作成者または店舗作成者以外のユーザーには削除ボタンが表示されないこと' do
+        another_user = FactoryBot.create(:profile)
+        sign_in(another_user.user)
+        visit store_path(@store)
+        expect(page).to have_no_css(".fa-trash-alt")
+      end
+    end  
   end
 end
