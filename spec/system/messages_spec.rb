5e9410b4a0887b5676658ae5eba7f05247951b11
fujii dan <2607dan@gmail.com>
2021-02-26 20:57:43 +0900

    message　結合テスト追加

--- spec/system/messages_spec.rb
+++ spec/system/messages_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe 'Messages', type: :system do
         visit community_messages_path(@community)
         expect(page).to have_no_content('送信')
       end
-      # docker環境でjs動かない為一旦コメントアウト
+      # docker環境でjs動かない為一旦メッセージアウト
       # it 'メッセージ投稿すると、ページ遷移無くメッセージが表示される', js: true do
       #   sign_in(@community.user)
       #   visit community_messages_path(@community)
@@ -49,21 +49,65 @@ RSpec.describe 'Messages', type: :system do
     before do
       @community = FactoryBot.create(:community)
       @profile = FactoryBot.create(:profile, user_id: @community.user_id)
-      FactoryBot.create_list(:message, 3, user_id: @community.user_id, community_id: @community.id)
+      FactoryBot.create(:message, user_id: @community.user_id, community_id: @community.id)
     end
 
-    it 'ユーザーアカウントが削除されるとそれに紐づくメッセージ情報も削除されること' do
-      sign_in(@community.user)
-      visit user_path(@community.user)
-      expect { click_on('アカウント削除') }.to change { @community.user.messages.count }.by(-3)
-      expect(current_path).to eq root_path
+    context 'メッセージ削除できる時' do
+      it 'メッセージ作成者には削除ボタンが表示されること' do
+        sign_in(@community.user)
+        visit community_messages_path(@community)
+        expect(page).to have_css(".fa-trash-alt")
+      end
+
+      it '店舗作成者は自身が作成していないメッセージでも削除ボタンが表示されること' do
+        @another_community = FactoryBot.create(:community)
+        another_profile = FactoryBot.create(:profile, user_id: @another_community.user_id)
+        FactoryBot.create(:message, community_id: @another_community.id, user_id: @community.user_id)
+        sign_in(@another_community.user)
+        visit community_messages_path(@another_community)
+        expect(page).to have_css(".fa-trash-alt")
+      end  
+
+      # # docker環境でjs動かない為一旦コメントアウト
+      # it 'メッセージ削除するとページ遷移なく画面に留まること', js: true  do
+      #   sign_in(@community.user)
+      #   visit community_messages_path(@community)
+      #   find(".fa-trash-alt").click
+      #   sleep(1)
+      #   expect(Message.count).to eq 0
+      #   expect(current_path).to eq community_messages_path(@community)
+      # end
+
+      it 'ユーザーアカウントが削除されるとそれに紐づくメッセージ情報も削除されること' do
+        FactoryBot.create_list(:message, 2, user_id: @community.user_id, community_id: @community.id)
+        sign_in(@community.user)
+        visit user_path(@community.user)
+        expect { click_on('アカウント削除') }.to change { @community.user.messages.count }.by(-3)
+        expect(current_path).to eq root_path
+      end
+
+      it 'コミュニティを削除するとそれに紐づくメッセージも削除されマイページに遷移すること' do
+        FactoryBot.create_list(:message, 2, user_id: @community.user_id, community_id: @community.id)
+        sign_in(@community.user)
+        visit communities_path
+        expect { click_on('削除する') }.to change { @community.messages.count }.by(-3)
+        expect(current_path).to eq user_path(@community.user)
+      end
     end
 
-    it 'コミュニティを削除するとそれに紐づくメッセージも削除されマイページに遷移すること' do
-      sign_in(@community.user)
-      visit communities_path
-      expect { click_on('削除する') }.to change { @community.messages.count }.by(-3)
-      expect(current_path).to eq user_path(@community.user)
+    context 'メッセージ削除できない時' do
+
+      it 'ログインしていないユーザーには削除ボタンが表示されないこと' do
+        visit community_messages_path(@community)
+        expect(page).to have_no_css(".fa-trash-alt")
+      end
+
+      it 'ログイン済みでもメッセージ作成者または店舗作成者以外のユーザーには削除ボタンが表示されないこと' do
+        another_user = FactoryBot.create(:profile)
+        sign_in(another_user.user)
+        visit community_messages_path(@community)
+        expect(page).to have_no_css(".fa-trash-alt")
+      end
     end
   end
 end
